6dde308b21083b235af41853c78d9ab489b5fe53
YASH-ROCKSTAR <yash.nigam_cs19@gla.ac.in>
2022-05-09 00:19:51 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
+
+
+//jsx instead of html
+// const name="Product";
+
+// const styling={
+  //   backgroundColor:"red",
+  //   padding:"20px"
+  // }
+    // // written inside return is html
+    // // style={styling}
+    // <>
+    //   <Navbar/>
+    //   <div>
+    //     Img
+    //     const
+    //     {/* {name} */}
+    //     {/* <div className='class' id='id'>
+
+    //     </div> */}
+    //     <div>
+    //       hello
+    //     </div>
+    //   price
+    //   </div>
+    // </>

88ee6eacb20d8670335c590e998eec75e30d8829
YASH-ROCKSTAR <yash.nigam_cs19@gla.ac.in>
2022-05-09 00:19:30 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+
+  </React.StrictMode>
+);
+
+    // {/* //ye component hai ismein phale letter capital hot ahai */}
+    // <App />
+    // <App />
+    // <App />
+    // <App />
